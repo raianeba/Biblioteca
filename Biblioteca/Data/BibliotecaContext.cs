0b44cbcf797b45ddcfae5b66bb1dbfa8bf047ed8
raianeba <raianebarroso@outlook.com>
2023-07-31 17:29:20 -0300

    Gerando

--- Biblioteca/Data/BibliotecaContext.cs
+++ Biblioteca/Data/BibliotecaContext.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Models;
 
 namespace Biblioteca.Data
 {
-    public class BibliotecaContext: DbContext
+    public class BibliotecaContext : DbContext
     {
-        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options)
+        public BibliotecaContext (DbContextOptions<BibliotecaContext> options)
+            : base(options)
         {
-        
-        } 
-        
+        }
+
+        public DbSet<Book> Book { get; set; } = default!;
+
+        public DbSet<Publisher> Publisher { get; set; } = default!;
+
+        public DbSet<Author> Author { get; set; } = default!;
+
     }
 }
---
+++ Biblioteca/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Biblioteca.Models
+{
+	[Table("Book")]
+	public class Book 
+	{
+		public int Id { get; set; }
+
+
+		public string? Title { get; set; }
+
+		public string? ISBN { get; set; }
+
+		public Publisher? Publisher { get; set; }
+
+		public ICollection <Author>? Authors { get; set; }
+
+	}
+}
